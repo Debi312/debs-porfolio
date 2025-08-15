77544227cbe3e5ddc77717fd4f15f2f5e46bfc45
Debi312 <deboragarciaescudero@gmail.com>
2025-08-15 08:08:28 +0200

    add personal information to AboutMe section

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import AboutMe from './components/AboutMe.tsx'
 import Projects from './components/Projects.tsx'
 import Technologies from './components/Technologies.tsx'
 import Layout from './components/Layout.tsx'
-import Navbar from './components/Navbar.tsx'
 import Header from './components/Header.tsx'
+import Footer from './components/Footer.tsx'
+
 function App() {
 
   return (
     <Layout>
-      <Navbar />
       <Header />
       <Projects />
       <AboutMe />
       <Technologies />
+      <Footer />
     </Layout>
   )
 }
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,12 +1,26 @@
 export default function AboutMe() {
     return (
-        <section id="about" className="min-h-screen flex flex-col items-center justify-center text-center">
+        <section
+            id="about"
+            className="min-h-screen flex flex-col items-center justify-center text-center mx-16"
+        >
             <h1 className="text-5xl md:text-6xl font-bold mb-4">
                 Sobre <span className="text-lavender">mi</span>
             </h1>
-            <p className="text-xl">
-                Desarrolladora web frontend enfocada en crear experiencias digitales limpias, funcionales y accesibles.
+
+            <p className="text-lg font-light leading-8">
+                Soy <b>desarrolladora frontend</b> con pasión por crear <b>experiencias digitales cuidadas al detalle</b>. Trabajo principalmente con el stack <b>MERN</b> (MongoDB, Express, React y Node.js), y disfruto especialmente de la parte
+                creativa: <b>transformar un diseño en una interfaz precisa y funcional</b>, replicando hasta el último píxel.
+                <br />
+                <br />
+                Antes de dedicarme a la programación, trabajé como <b>técnica de laboratorio</b>, una etapa que me enseñó a ser <b>meticulosa</b>, <b>paciente</b> y a <b>cuidar cada detalle</b>, habilidades que hoy aplico en cada proyecto que desarrollo.
+                <br />
+                <br />
+                He participado en <b>dos programas de entrenamiento intensivo</b> en entornos simulados de empresa con <b>Igrowker</b>, donde en el último tuve la oportunidad de <b>liderar la parte de frontend</b>, guiando al equipo para <b>alcanzar los objetivos</b> y <b>resolviendo problemas</b> de forma colaborativa.
+                <br />
+                <br />
+                Me motiva afrontar <b>nuevos desafíos</b>, aprender constantemente y <b>explorar nuevas tecnologías</b>. Actualmente sigo ampliando conocimientos y creando proyectos que combinen <b>creatividad</b>, <b>rendimiento</b> y una <b>excelente experiencia de usuario</b>.
             </p>
         </section>
-    )
+    );
 }
