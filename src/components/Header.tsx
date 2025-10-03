26a9b05af219fab9fef32491389d2d0c3de93ae6
Debi312 <deboragarciaescudero@gmail.com>
2025-10-03 21:39:36 +0200

    style: improve appearance of Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,28 @@
 export default function Header() {
-    return (
-        <section id="home" className="min-h-screen flex flex-col items-center justify-center text-center">
-            <h1 className="text-5xl md:text-6xl font-bold mb-4">
-                Hola, soy <span className="text-lavender">Débora</span>
-            </h1>
-            <p className="text-xl">
-                Desarrolladora web frontend enfocada en crear experiencias digitales limpias, funcionales y accesibles.
-            </p>
-        </section>
-    )
+  return (
+    <section
+      id="home"
+      className="
+        relative isolate
+        min-h-screen flex flex-col items-center justify-center text-center
+      "
+    >
+      <div
+        className="
+          pointer-events-none
+          absolute top-0 left-1/2 -translate-x-1/2
+          w-[100vw] h-full
+          bg-[url('/background_4.png')] bg-cover bg-center bg-no-repeat
+          -z-10
+        "
+      />
+
+      <h1 className="text-6xl lg:text-7xl font-bold mb-14">
+        Hola, soy <span className="text-lavender">Débora</span>
+      </h1>
+      <p className="font-courier text-navy font-[500] text-2xl ">
+        &lt; Diseño y desarrollo productos digitales funcionales, accesibles e intuitivos /&gt;
+      </p>
+    </section>
+  )
 }
